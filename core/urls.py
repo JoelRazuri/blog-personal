f65bf568c1b5ebd6b993a1b0c2fb1ee86f371f72
Joel Razuri <jrazuri98@hotmail.com>
2024-03-22 14:18:30 -0300

    estructura del proyecto

---
+++ core/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('blog/', include('blog.urls', namespace='blog')),
+    path('accounts/', include('accounts.urls', namespace='accounts')),
+    path('comments/', include('comments.urls', namespace='comments')),
+]
