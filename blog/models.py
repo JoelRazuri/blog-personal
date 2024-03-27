a626fd1a8f63ab25b71b48377db33e357a0bdf2e
Joel Razuri <jrazuri98@hotmail.com>
2024-03-26 22:10:37 -0300

    corrigiendo blog/models

--- blog/models.py
+++ blog/models.py
@@ -6,9 +6,9 @@ from datetime import date
 class Post(models.Model):
     title = models.CharField(max_length=20, blank=False, null=False, verbose_name='Título')
     content = models.TextField(blank=False, null=False, verbose_name='Contenido')
-    image = models.ImageField(upload_to='post/' ,verbose_name='Imagen')
-    created_date = models.DateField(default=date.today)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='post_images/' ,verbose_name='Imagen')
+    created_date = models.DateField(default=date.today, verbose_name='Fecha de creación')
+    author = models.ForeignKey(User, on_delete=models.CASCADE,erbose_name='Autor')
 
     def __str__(self):
         return self.title
