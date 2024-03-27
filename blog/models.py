972601c7f042c81e9f10508b4fb805c472673c7e
Joel Razuri <jrazuri98@hotmail.com>
2024-03-26 22:32:11 -0300

    corrigiendo error migrations

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import User
+from accounts.models import CustomUser
 from django.db import models
 from datetime import date
 
@@ -8,7 +8,7 @@ class Post(models.Model):
     content = models.TextField(blank=False, null=False, verbose_name='Contenido')
     image = models.ImageField(upload_to='post_images/' ,verbose_name='Imagen')
     created_date = models.DateField(default=date.today, verbose_name='Fecha de creación')
-    author = models.ForeignKey(User, on_delete=models.CASCADE,erbose_name='Autor')
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, verbose_name='Autor')
 
     def __str__(self):
         return self.title
