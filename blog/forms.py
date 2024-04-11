090d9a5d68bf7f3d8261d3db4eeee5c9fc6c59ab
Joel Razuri <jrazuri98@hotmail.com>
2024-04-10 22:06:37 -0300

    widget form

--- blog/forms.py
+++ blog/forms.py
@@ -1,8 +1,13 @@
 from django.forms import ModelForm
 from .models import Post
+from django import forms
 
 
 class PostForm(ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'content', 'image')
+        widgets = {
+            'title': forms.TextInput(attrs={'placeholder': 'titulo'}),
+            'content': forms.TextInput(attrs={'placeholder': 'contenido'}),
+        }
