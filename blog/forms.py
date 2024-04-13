472333d0354ba8ea0b55f937c8e19d27fd8d06bc
Joel Razuri <jrazuri98@hotmail.com>
2024-04-12 22:50:58 -0300

    widget forms

--- blog/forms.py
+++ blog/forms.py
@@ -8,6 +8,7 @@ class PostForm(ModelForm):
         model = Post
         fields = ('title', 'content', 'image')
         widgets = {
-            'title': forms.TextInput(attrs={'placeholder': 'titulo'}),
-            'content': forms.TextInput(attrs={'placeholder': 'contenido'}),
+            'title': forms.TextInput(attrs={'placeholder': 'titulo', 'class': 'w-full'}),
+            'content': forms.Textarea(attrs={'placeholder': 'contenido', 'class': 'w-full h-[100%]'}),
+            'image': forms.FileInput(),
         }
