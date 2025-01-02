cae8f23fe1a302b6454be69e59195415e85f3a84
Joel Razuri <jrazuri98@hotmail.com>
2025-01-02 15:24:17 -0300

    config para desarrollo con tailwind

--- core/settings.py
+++ core/settings.py
@@ -34,7 +34,7 @@ INSTALLED_APPS = [
 
     'tailwind',
     'theme',
-    # 'django_browser_reload',
+    'django_browser_reload',
     'ckeditor',
 
     'blog',
@@ -59,7 +59,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    # "django_browser_reload.middleware.BrowserReloadMiddleware", # Tailwind middleware
+    "django_browser_reload.middleware.BrowserReloadMiddleware", # Tailwind middleware
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
@@ -88,7 +88,10 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": dj_database_url.config(default=os.getenv("DATABASE_URL"))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
