83a4b88411efe84ecbf531a992373daed2b59900
Joel Razuri <jrazuri98@hotmail.com>
2025-01-25 14:03:55 -0300

    migraciones despues de iniciar el servidor

--- core/wsgi.py
+++ core/wsgi.py
@@ -8,9 +8,19 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
-
+import django
 from django.core.wsgi import get_wsgi_application
+from django.core.management import call_command
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
+# Inicializa la aplicación WSGI
 application = get_wsgi_application()
+
+# Aplica las migraciones automáticamente
+try:
+    django.setup()  # Configura Django antes de usar comandos de administración
+    call_command('migrate', interactive=False)
+    print("Migraciones aplicadas correctamente.")
+except Exception as e:
+    print(f"Error al aplicar migraciones: {e}")
