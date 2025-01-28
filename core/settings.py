ed3f0beb2ca81a226f82b8bfcbc2cea64b494b0d
Joel Razuri <jrazuri98@hotmail.com>
2025-01-28 15:15:10 -0300

    correcion nombre de bucket

--- core/settings.py
+++ core/settings.py
@@ -180,6 +180,6 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = '<blog-personal-media>'
+AWS_STORAGE_BUCKET_NAME = 'blog-personal-media'
 AWS_S3_REGION_NAME = 'us-east-2'  # Cambia a tu región si es diferente
 AWS_QUERYSTRING_AUTH = False  # Hace que las URL sean públicas
