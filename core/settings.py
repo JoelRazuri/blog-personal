75199cf6243b80320abf5e6fcbe21ba404274fd3
Joel Razuri <jrazuri98@hotmail.com>
2025-01-28 15:04:01 -0300

    config AWS S3

--- core/settings.py
+++ core/settings.py
@@ -174,3 +174,12 @@ SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Configuración para AWS
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = '<blog-personal-media>'
+AWS_S3_REGION_NAME = 'us-east-2'  # Cambia a tu región si es diferente
+AWS_QUERYSTRING_AUTH = False  # Hace que las URL sean públicas
