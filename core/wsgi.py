e8df2a003789416051be0e1f0d1e73abe95ba61e
Joel Razuri <jrazuri98@hotmail.com>
2025-03-07 10:58:30 -0300

    borrando codigo de superusuario

--- core/wsgi.py
+++ core/wsgi.py
@@ -12,21 +12,6 @@ import django
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-django.setup()
-
-from django.contrib.auth import get_user_model
-
-# Crear superusuario si no existe
-User = get_user_model()
-username = "admin"
-email = "admin@example.com"
-password = "admin"
-
-if not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username, email, password)
-    print("Superusuario creado con éxito.")
-else:
-    print("El superusuario ya existe.")
 
 # Inicializa la aplicación WSGI
 application = get_wsgi_application()
